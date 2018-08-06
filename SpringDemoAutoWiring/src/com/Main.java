441553e924c73d075210213e9752c043f96f5f7a
mamtaharris <mamtamariamharris@gmail.com>
2018-08-06 02:09:17 -0700

    Auto Wiring

---
+++ SpringDemoAutoWiring/src/com/Main.java
@@ -0,0 +1,18 @@
+package com;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+public class Main {
+	public static void main(String[] args) {
+				
+		Resource resource = new ClassPathResource("beans.xml");
+		BeanFactory factory = new XmlBeanFactory(resource);
+		Employee employee = (Employee) factory.getBean("emp");
+		System.out.println(employee);
+		}
+}
+
+ 
